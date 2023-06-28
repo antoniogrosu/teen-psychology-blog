60d01fd1fabe22a31817630e4c39e0ee8f1bbb60
Antonio Grosu <antonio.webdesign19@gmail.com>
2023-06-29 00:19:51 +0300

    final version

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,16 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-async function getUsersWithAdminRole() {
-  const res = await fetch(
-    "https://blog-d9dcf-default-rtdb.europe-west1.firebasedatabase.app/data/admins.json"
-  );
-  const data = await res.json();
-  console.log(data);
-  const admins = Object.values(data).map((admin) => admin.email);
-  return admins;
-}
-
 export default NextAuth({
   providers: [
     GoogleProvider({
