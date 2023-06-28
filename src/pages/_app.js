3383c090d76f040bb659d85da78ed603a01f19e9
Antonio Grosu <antonio.webdesign19@gmail.com>
2023-06-29 00:09:45 +0300

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import "src/styles/globals.css";
 import Navbar from "src/components/navbar";
 import Footer from "src/components/footer";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+
 import { SessionProvider } from "next-auth/react";
 export default function App({ Component, pageProps, session }) {
   return (
@@ -15,6 +17,7 @@ export default function App({ Component, pageProps, session }) {
         <link rel="apple-touch-icon" href="/Logo.svg" />
       </Head>
       <Navbar />
+      <Analytics />
       <Component {...pageProps} />
       <Footer />
     </SessionProvider>
